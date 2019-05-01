838c7888e3dd5a267ccaa4ccfb02ee2c387378ca
ThanaponWi <ooowdcd@hotmail.com>
2019-05-01 23:03:55 +0700

    pond2

---
+++ TrainCal/app/src/main/java/com/example/traincal/TrainItem.java
@@ -0,0 +1,15 @@
+package com.example.traincal;
+
+public class TrainItem {
+    private String mTrainName;
+    private int mTrainImage;
+
+    public TrainItem(String TrainName,int TrainImage)
+    {
+        this.mTrainName = TrainName;
+        this.mTrainImage = TrainImage;
+    }
+    public  String getmTrainName(){return mTrainName;}
+    public int getmTrainImage(){return mTrainImage;}
+
+}
